48f463aab55803ef4d34887bb34652738e3beb6c
N4rdelli <nardellianelize20@gmail.com>
2023-08-11 14:03:03 -0300

    Switch e Links finalizados.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,20 +1,31 @@
-import "./App.css";
-
+import { useState } from "react";
+import Switch from "../components/switch/switch";
 import Profile from "../components/profile/profile";
+import Links from "../components/links/links";
+
+import "./App.css";
 
 import imgDarkMode from "../img/avatar.jpg"
 import imgLightMode from "../img/avatar2.jpg"
-import { useState } from "react";
 
 const App = () => {
-    
+
     const [isLightMode,setIsLightMode] = useState(true)
     const imgSrc = isLightMode ? imgLightMode : imgDarkMode
     const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro";
 
     return(
-        <div>
+        <div id="container">
             <Profile imgSrc={imgSrc} imgAlt={imgAlt} >@aa_nardelli</Profile>
+            <Switch></Switch>
+            <div id="links">
+                <ul>
+                    <Links link={"https://github.com"}>GitHub</Links>
+                    <Links link={"https://instagram.com/aa_nardelli?utm_source=qr&igshid=ZDc4ODBmNjlmNQ%3D%3D"}>Instagram</Links>
+                    <Links link={"http://tiktok.com/@heynardelli"}>TikTok</Links>
+                    <Links link={"#"}>Portifólio</Links>
+                </ul>
+            </div>
         </div>
     )
 }
